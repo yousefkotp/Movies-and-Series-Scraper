35c1b95174b45e2ff91ccb0a78e7bbba806bf278
Yousef Kotp <yousefkotp@outlook.com>
2022-10-21 12:16:26 +0200

    Changed the Source

--- Movies & Series Scraper.py
+++ Movies & Series Scraper.py
@@ -13,31 +13,35 @@ while True:
 if option == 1:                      # Watch a series
     series_name = input("Enter series name: ")
     series_name.rstrip(" ")
-    series_name = series_name.replace(" ", "-")
 
     # Preparing our html page and soup
-    html_page = requests.get("https://riko.egybest.asia/explore/?q=" + series_name)
+    html_page = requests.get("https://mycima.tube/search/"+series_name+"/list/series")
     soup = BeautifulSoup(html_page.content, "lxml")
 
     # Scraping links and titles of series resulted from the query
-    links_of_recommended_series = soup.find_all("a", {"class": "movie"})
-    titles_of_recommended_series = soup.find_all("span", {"class": "title"})
+    recommended_series = soup.find_all("div", {"class": "Thumb--GridItem"})
+    links_of_recommended_series = []
+
     print("Which series do you want to watch?")
 
     counter_series = 1
-    for i in titles_of_recommended_series:
-        print(str(counter_series)+"- "+i.text)
+    for i in recommended_series:
+        link = i.find("a")
+        links_of_recommended_series.append(link.get("href"))
+        print(str(counter_series)+"- "+link.get("title"))
         counter_series += 1
+
     series_number = int(input("Enter the number corresponding to your wanted series: "))
 
     # Preparing html page and soup for desired series
-    html_page = requests.get(links_of_recommended_series[series_number-1].get("href"))
+    html_page = requests.get(links_of_recommended_series[series_number-1])
     soup = BeautifulSoup(html_page.content, "lxml")
 
+
     # Scraping links and titles of seasons of the desired series
-    temp_list = soup.find("div", {"class": "contents movies_small"})
+    temp_list = soup.find("div", {"class": "List--Seasons--Episodes"})
     seasons_links = temp_list.findAll("a")          # i.get("href")
-    seasons_links.reverse()
+
 
     season = int(input("There are "+str(len(seasons_links))+" Seasons, Enter your desired season number: "))
 
@@ -45,51 +49,53 @@ if option == 1:                      # Watch a series
     html_page = requests.get(seasons_links[season-1].get("href"))
     soup = BeautifulSoup(html_page.content, "lxml")
 
+
     # Scraping episodes links and titles
-    temp_list = soup.find("div", {"class": "movies_small"})
+    temp_list = soup.find("div", {"class": "Episodes--Seasons--Episodes"})
     episodes_links = temp_list.findAll("a")
     episodes_links.reverse()
 
     print("There are " + str(len(episodes_links)) + " episodes")
     ep1 = int(input("Enter episode number (from): "))
     ep2 = int(input("Enter episode number (to): "))
-
     counter_episodes = ep2
     while counter_episodes >= ep1:      # Iterating through desired episodes
         html_page = requests.get(episodes_links[counter_episodes-1].get("href"))
         soup = BeautifulSoup(html_page.content, "lxml")
-        link = soup.find("iframe", {"class": "auto-size"})
-        watching_link = link.get("src")
-        result = requests.get("https://riko.egybest.asia" + watching_link)
-        webbrowser.open("https://riko.egybest.asia" + watching_link)
+        link = soup.find("iframe",{"name":"watch"})
+        watching_link = link.get("data-lazy-src")
+        result = requests.get(watching_link)
+        webbrowser.open(watching_link)
         counter_episodes -= 1
 
 elif option == 2:       # Watch a movie
     movie_name = input("Enter movie name: ")
     movie_name.rstrip(" ")
-    movie_name = movie_name.replace(" ", "%20")
 
-    html_page = requests.get("https://riko.egybest.asia/explore/?q="+movie_name)
+    html_page = requests.get("https://mycima.tube/search/"+movie_name)
     soup = BeautifulSoup(html_page.content, "lxml")
 
-    movies_links = soup.find_all("a", {"class": "movie"})
-    movies_titles = soup.find_all("span", {"class": "title"})
-    print("What movies do you want to watch?")
+    recommended_movies = soup.find_all("div", {"class": "Thumb--GridItem"})
+    links_of_recommended_movies= []
 
-    counter = 1
-    for i in movies_titles:
-        print(str(counter)+"- "+i.text)
-        counter += 1
+    print("Which Movie do you want to watch?")
 
-    movie_number = int(input("Please enter the number of the movie you want: "))
-    movie_link = movies_links[movie_number-1].get("href")
+    counter_series = 1
+    for i in recommended_movies:
+        link = i.find("a")
+        links_of_recommended_movies.append(link.get("href"))
+        print(str(counter_series) + "- " + link.get("title"))
+        counter_series += 1
+
+    movie_number = int(input("Enter the number corresponding to your wanted movie: "))
+    movie_link = links_of_recommended_movies[movie_number-1]
 
     html_page = requests.get(movie_link)
     soup = BeautifulSoup(html_page.content, "lxml")
 
-    link = soup.find("iframe", {"class": "auto-size"})
-    watching_link = link.get("src")
-    webbrowser.open("https://riko.egybest.asia" + watching_link)
+    link = soup.find("iframe", {"name": "watch"})
+    watching_link = link.get("data-lazy-src")
+    webbrowser.open(watching_link)
 
 
 else:       # Download a whole season
@@ -108,7 +114,7 @@ else:       # Download a whole season
     soup = BeautifulSoup(html_page.content, "lxml")
     x = soup.find('ul', {'class': "Season--Download--Mycima--Single"})
     if x is None:
-        print("This feature is not available for "+str(series_name)+" season "+str(season)+"!")
+        input("This feature is not available for "+str(series_name)+" season "+str(season)+"! \nPress any key to exit")
         sys.exit()
 
 
